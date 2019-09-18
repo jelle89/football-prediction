a5c179b34cf0bca886d81e213bbd5e4dbff7e237
jelle89 <jelle.monen@hotmail.com>
2019-09-18 21:14:20 +0200

    fixed capital letters

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import signIn from "./components/signIn";
-import footballPage from "./components/footballPage";
+import SignIn from "./components/SignIn";
+import FootballPage from "./components/FootballPage";
 
 
 
@@ -12,8 +12,8 @@ class App extends Component {
       <BrowserRouter >
       <div className="App">
         <Switch>
-          <Route exact path="/" component={signIn} />
-          <Route exact path="/test" component={footballPage} />
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/test" component={FootballPage} />
         </Switch>
       </div>
       </BrowserRouter>
